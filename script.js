490c06a6feddd2eac9f8146db7d7d4d8322d8880
Edin Purivatra <edinpurivatra@gmail.com>
2019-03-31 14:39:06 +0200

    user friendly

--- script.js
+++ script.js
@@ -16,6 +16,13 @@ function submit () {
       token = JSON.parse(http.responseText);
       console.log(token.token);
       document.getElementById('button').style = 'display:block';
+      document.getElementById('error').style = 'display:none';
+      clearInput();
+    }
+    if(http.readyState == 4 && http.status != 200){
+      document.getElementById('error').style = 'display:block';
+      document.getElementById('error').innerHTML = `Pogresan email ili password, pokusajte ponovo!`;
+      clearInput();
     }
   }
   http.open("POST", 'https://3d1pftib26.execute-api.eu-west-1.amazonaws.com/dev/user/login', true);
@@ -24,6 +31,7 @@ function submit () {
 }
 
 function show () {
+  let information;
   let http = new XMLHttpRequest;
   let url = "https://3d1pftib26.execute-api.eu-west-1.amazonaws.com/dev/user/profile";
   http.open('GET' , url,true);
@@ -32,6 +40,19 @@ function show () {
   http.onreadystatechange = function () {
     if(http.readyState==4 && http.status==200){
       console.log(http.responseText);
+      document.getElementById('button').style = 'display:none';
+      information = JSON.parse(http.responseText);
+      let table = document.getElementsByTagName('tbody')[0];
+      console.log(table);
+      document.getElementById('information').style = 'display:block';
+      table.innerHTML += `<tr><td>${information.first_name}</td><td>${information.last_name}</td><td>${information.email}</td><td>${information.id}</td></tr>`
     }
+  }
+}
+
+function clearInput() {
+  let inputs = document.getElementsByTagName('input');
+  for(let i = 0; i < inputs.length; i++){
+    inputs[i].value = '';
   }
 }
